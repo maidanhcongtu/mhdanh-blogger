4526f7c6812205337a9eac846cb5353e1708403e
mai <okman@gmail.com>
2017-08-20 12:15:41 +0700

    finish basic for route

--- project/angular1/app/app.js
+++ project/angular1/app/app.js
@@ -1,4 +1,17 @@
 (function(){
 	"use strict";
-	var app = angular.module("App",["MyCtrl"]);
+	var app = angular.module("App",[
+		"ngRoute",
+		"MyCtrl"
+		])
+		.config(["$routeProvider", function($routeProvider) {
+			$routeProvider
+				.when("/san-pham", {
+					template: "<div>Danh sách sản phẩm</div>",
+					controller: "ListSanPhamCtrl"
+				}).when("/san-pham/:id", {
+					template: "<div>Sản phẩm chi tiết {{idSanPham}}</div>",
+					controller: "SanPhamDetailCtrl"
+				});
+		}]);
 })();
--- project/angular1/app/main.ctrl.js
+++ project/angular1/app/main.ctrl.js
@@ -3,5 +3,11 @@
 	angular.module("MyCtrl",[])
 		.controller("AppCtrl", ["$scope", function($scope){
 			$scope.sayHello = "Welcome angular 1";
-		}]);
+		}])
+		.controller("ListSanPhamCtrl", ["$scope", function($scope){
+
+		}])
+		.controller("SanPhamDetailCtrl", ["$scope", "$routeParams", function($scope, $routeParams){
+			$scope.idSanPham = $routeParams.id;
+		}]);;
 })();
