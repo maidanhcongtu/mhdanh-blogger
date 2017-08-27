c7298029f9b0595044ce705c2b2995f54350b0d6
mai <okman@gmail.com>
2017-08-27 11:05:09 +0700

    P3 - multilingual angular1

--- project/angular1/app/app.js
+++ project/angular1/app/app.js
@@ -1,9 +1,17 @@
 (function(){
 	"use strict";
 	var app = angular.module("App",[
-		"ngRoute",
+		"ngRoute", // angular-route
+		"pascalprecht.translate", // angular-translate
+		"ngSanitize", // angular-sanitize,
+		"ngCookies", // angular-storage,
 		"MyCtrl"
 		])
+		.filter("htmlSafe", ['$sce', function($sce) {
+		    return function(htmlCode){
+		        return $sce.trustAsHtml(htmlCode);
+		    };
+		}])
 		.config(["$routeProvider", function($routeProvider) {
 			$routeProvider
 				.when("/san-pham", {
@@ -13,5 +21,29 @@
 					templateUrl: "app/views/san-pham/san-pham-detail.html",
 					controller: "SanPhamDetailCtrl"
 				});
+		}])
+		.config(['$translateProvider', function ($translateProvider) {
+			$translateProvider.useStaticFilesLoader({
+				prefix:"app/asserts/languages/",
+				suffix:".json"
+			})
+			// Enable escaping param of HTML
+  			$translateProvider.useSanitizeValueStrategy('escapeParameters');
+			$translateProvider.preferredLanguage('vi');
+			$translateProvider.useLocalStorage();
+		}])
+		.run(["$rootScope", "$translate", "$location", function($rootScope, $translate, $location) {
+		    $rootScope.$on('$routeChangeStart', function (event, next) {
+		    	// find param lang in url
+		    	var param = $location.search();
+		    	if(param.lang != undefined) {
+			    	var lang = "vi";
+			    	if(["vi","en"].indexOf(param.lang) != -1) {
+			    		lang = param.lang;
+			    	}
+			    	$translate.use(lang);
+		    	}
+		    });
 		}]);
+
 })();
